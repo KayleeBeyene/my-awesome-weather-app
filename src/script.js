f799a8530d34e01e2e0c33e94368c87868a4dbc4
KayleeBeyene <100857554+KayleeBeyene@users.noreply.github.com>
2022-03-05 16:27:43 -0700

    Display forecast data using api

--- src/script.js
+++ src/script.js
@@ -49,6 +49,8 @@ function showTemperature(response) {
   let cityName = response.data.name;
   let city = document.querySelector("#city-name");
   city.innerHTML = cityName;
+
+  getForecast(response.data.coord);
 }
 function search(event) {
   event.preventDefault();
@@ -88,36 +90,55 @@ function showFahrenheitTemperature(event) {
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
   console.log(celsiusTemperature);
 }
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
-
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+  return days[day];
+}
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
         </div>
       </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
 }
-
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "5306333b3e62e52ba951ab4ebf05f12e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 let celsiusTemperature = null;
 
 let searchInput = document.querySelector("#search-form");
@@ -133,4 +154,3 @@ let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
 
 searchCity("Calgary");
-displayForecast();
